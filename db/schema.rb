8d8e8d9cc17cbd67b372e34675c77135acdfa3d0
lisamarie616 <lisamarie616@gmail.com>
2017-02-22 20:26:17 -0800

    add categorizations join table and declare model relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223040024) do
+ActiveRecord::Schema.define(version: 20170223042246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170223040024) do
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "categorizations", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
