a6b2996ed73f6c6c07bc1f23e4d0a0b81a5bd38a
lisamarie616 <lisamarie616@gmail.com>
2017-03-01 17:46:23 -0800

    edit admin panel

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -27,6 +27,8 @@ RailsAdmin.config do |config|
   ## To disable Gravatar integration in Navigation Bar set to false
   # config.show_gravatar true
 
+  config.included_models = ["Category", "Product"]
+
   config.actions do
     dashboard                     # mandatory
     index                         # mandatory
